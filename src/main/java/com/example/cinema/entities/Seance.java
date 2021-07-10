a53a658870cdccb87faddc4e3def478375956f11
OussamaHannani <ohannani@gmail.com>
2021-07-10 01:04:18 +0100

    comm 1

---
+++ src/main/java/com/example/cinema/entities/Seance.java
@@ -0,0 +1,17 @@
+package com.example.cinema.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.Date;
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor @ToString
+public class Seance {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Temporal(TemporalType.TIME)
+    private Date heureDebut;
+}
---
+++ src/main/java/com/example/cinema/web/CinemaRestController.java
@@ -0,0 +1,58 @@
+package com.example.cinema.web;
+
+import com.example.cinema.dao.FilmRepository;
+import com.example.cinema.dao.TicketRepository;
+import com.example.cinema.entities.Film;
+import com.example.cinema.entities.Ticket;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import javax.transaction.Transactional;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+public class CinemaRestController {
+    @Autowired
+    private FilmRepository filmRepository;
+    @Autowired
+    private TicketRepository ticketRepository;
+    @GetMapping (path = "/imageFilm/{id}", produces = MediaType.IMAGE_JPEG_VALUE)
+    public byte[] image(@PathVariable (name = "id")Long id) throws Exception{
+        Film f=filmRepository.findById(id).get();
+        String photoName=f.getPhoto();
+        File file=new File(System.getProperty("user.home")+"/cinema/images/"+photoName);
+        Path path= Paths.get(file.toURI());
+        return Files.readAllBytes(path);
+
+    }
+
+    @PostMapping("/payerTickets")
+    @Transactional
+    public List<Ticket> payerTickets(@RequestBody TicketFrom ticketFrom){
+        List<Ticket> listTickets=new ArrayList<>();
+    ticketFrom.getTickets().forEach(idTicket ->{
+        Ticket ticket=ticketRepository.findById(idTicket).get();
+        ticket.setNomClient(ticketFrom.getNomClient());
+        ticket.setReserve(true);
+        ticket.setCodePayement(ticketFrom.getCodePayement());
+        ticketRepository.save(ticket);
+        listTickets.add(ticket);
+    });
+    return listTickets;
+
+    }
+}
+@Data
+class TicketFrom {
+    private String nomClient;
+    private int codePayement;
+    private List<Long> tickets = new ArrayList<>();
+}
